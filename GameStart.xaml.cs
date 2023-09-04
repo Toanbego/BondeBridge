866cebbe2680b39e63636c83e1a96cb4729a12b8
toanbego <toanbego@gmail.com>
2023-09-04 15:02:29 +0200

    Current layout is finished. Need to add functionality for calculating user inputs and score

---
+++ GameStart.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BondeBridge
+{
+    /// <summary>
+    /// Interaction logic for GameStart.xaml
+    /// </summary>
+    public partial class GameStart : UserControl
+    {
+        public GameStart()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void start_game_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            window.Content = new GameConfig();
+       
+        }
+
+        private void quit_game_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BondeBridge
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int currentScore { get; set; }
+        public int MyPrediction { get; set; }
+        public int MyTricks{ get; set; }
+        public int TotalScore{ get; set; }
+        public bool isDealer { get; set; }
+        public bool isLast { get; set; }
+
+
+        public Player(string Name) 
+        {
+            this.Name = Name;
+            this.currentScore = 0;
+            this.TotalScore = 0;
+        }
+
+        public void CalculateScore(int currentRound)
+        {
+            if(MyPrediction == MyTricks)
+            {
+                currentScore = MyTricks + 10;
+            }
+            else { currentScore = MyTricks; }
+            
+            TotalScore += currentScore;
+        }
+
+    }
+}
